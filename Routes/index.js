b39027c0d4656159e310a9eea5bab0b184a35444
arvindantal01 <arvindantal01@gmail.com>
2023-04-11 18:14:44 +0530

    second

--- Routes/index.js
+++ Routes/index.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 const cors = require('cors')
 const path = require('path');
 const { save_advance_info } = require("../API/AdminAD/advance");
+const  {User}  = require("../API/User/user");
 var router = express.Router();
 var jsonParser = bodyParser.json();
 router.use(jsonParser)
@@ -26,4 +27,8 @@ const corsOpts = {
 router.use(cors(corsOpts));
 // router.use(express.static(__dirname + '../uploads/'))
 router.get('/save_advance_info',save_advance_info)
+router.get('/get_advance_info',async(req,res)=>{
+    const advance =await User.test()
+    res.json({advance})
+})
 module.exports = router;
