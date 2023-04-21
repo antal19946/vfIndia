da832b2257124f15bbe2dc1dd3f613a192f33d47
arvindantal01 <arvindantal01@gmail.com>
2023-04-21 18:06:54 +0530

    office 2

--- controller/commans/levelDistribution.js
+++ controller/commans/levelDistribution.js
@@ -15,8 +15,15 @@ class Distribution{
             if (!spo) {
                 break
             }
-             const update = await userWallet.findOneAndUpdate({user_Id:spo.user_Id,},)
-            console.log(spo)
+            const wallet = await userWallet.findOne({user_Id:spo.user_Id})
+            const count_in_wallet = wallet.level_income.count_in_wallet;
+            if (count_in_wallet!=null) {
+                const wall = wallet[count_in_wallet]
+                const update = await userWallet.findOneAndUpdate({user_Id:wallet.user_Id},{'[count_in_wallet].value':wallet[count_in_wallet].value+10,'level_income.value':wallet.level_income.value+10})
+                console.log(wall.value)
+            } else {
+                console.log('test')
+            }
             sponsor = spo.sponsor_Id
         }
     }
